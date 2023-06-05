0111a26b5f5a37176e0808a7587ed63cce2a0719
allanthuranira <allanthuranira>
2023-06-05 14:13:00 +0300

    positive only

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var arr0 = [-2, 3, 5, -6, -9, 7, 8, -12];
-var arr1 = [0.5, 7.3, 2, 4, 7, 0.9, 1.35];
-let sum = 0;
+var myArray = [4, -5, 0, 2, -67, 8, 10, -34];
+var result = myArray.filter(function (number) {
+    return number > 0;
+});
+console.log(result);
 
-for (let i = 0; i < arr1.length; i++) {
-    sum += arr1[i];
-}
-
-console.log(sum);
